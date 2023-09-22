9158c4322519d6c5cc05b060f34f2d82e9eccbf9
NativBashari <nativbas2000@gmail.com>
2023-09-22 13:43:58 +0300

    project_setup

---
+++ Logic/Repository/FundamentalsLogic.cs
@@ -0,0 +1,13 @@
+﻿using Logic.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Repository
+{
+    public class FundamentalsLogic : IFundamentalsLogic 
+    {
+    }
+}
---
+++ RuleNumberOne_Server/Controllers/FundamentalsController.cs
@@ -0,0 +1,25 @@
+﻿using Logic.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RuleNumberOne_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FundamentalsController : ControllerBase
+    {
+        
+        public FundamentalsController(IFundamentalsLogic)
+        {
+
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<string> GetFundamentalsAsync(string id)
+        {
+
+        }
+
+    }
+}

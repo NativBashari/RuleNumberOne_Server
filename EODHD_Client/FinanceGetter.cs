7e6e57ae48dce43bea83c0ae243236578f2b3899
NativBashari <nativbas2000@gmail.com>
2023-09-22 13:07:55 +0300

    initialize

---
+++ EODHD_Client/FinanceGetter.cs
@@ -0,0 +1,22 @@
+﻿using Entities.RawModels;
+namespace EODHD_Client
+{
+    public class FinanceGetter  
+    {
+        private HttpClient _httpClient;
+        private string _dataProviderEP;
+        private string _APIKey;
+        public FinanceGetter(string dataProviderEP, string apiKey)
+        {
+            _dataProviderEP = dataProviderEP;
+            _APIKey = apiKey;
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<FinancialData>> GetFinancialDatasAsync(string stockID)
+        {
+             return new List<FinancialData>();
+        }
+
+    }
+}
